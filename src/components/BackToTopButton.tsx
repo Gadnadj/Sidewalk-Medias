18addc3987f05d102229eef032df936404010315
Gad Nadjar <gadnadjar@gmail.com>
2025-01-23 13:28:46 +0200

    move button

--- src/components/BackToTopButton.tsx
+++ src/components/BackToTopButton.tsx
@@ -18,7 +18,7 @@ const BackToTopButton = () => {
 
     return (
         show && (
-            <button onClick={() => scrollToTop()} className='bg-accent w-12 h-12 hover:bg-accent-hover text-white rounded-full fixed right-3 bottom-3 cursor-pointer flex justify-center items-center transition-all'>
+            <button onClick={() => scrollToTop()} className='bg-accent w-12 h-12 hover:bg-accent-hover text-white rounded-full fixed right-6 bottom-6 cursor-pointer flex justify-center items-center transition-all'>
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
                     <path strokeLinecap="round" strokeLinejoin="round" d="m4.5 15.75 7.5-7.5 7.5 7.5" />
                 </svg>
